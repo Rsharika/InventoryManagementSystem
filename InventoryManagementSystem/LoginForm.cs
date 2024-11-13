12dff29a7f486c702b690f18705ee1d91afd9193
Rsharika <43790169+Rsharika@users.noreply.github.com>
2024-11-14 00:43:18 +0600

    Add files via upload

---
+++ InventoryManagementSystem/LoginForm.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryManagementSystem
+{
+    public partial class LoginForm : Form
+    {
+        //SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ASUS\Downloads\Compressed\InventoryManagementSystem\InventoryManagementSystem\Tutorial Database\dbIMS.mdf;Integrated Security=True;Connect Timeout=30");
+
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ASUS\Desktop\Project\InventoryManagementSystem\InventoryManagementSystem\Database\dbIMS.mdf;Integrated Security=True;Connect Timeout=30");
+        SqlCommand cm = new SqlCommand();
+        SqlDataReader dr;
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBoxPass_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxPass.Checked == false)
+                txtPass.UseSystemPasswordChar = true;
+            else
+                txtPass.UseSystemPasswordChar = false;
+        }
+
+        private void lblClear_Click(object sender, EventArgs e)
+        {
+            txtName.Clear();
+            txtPass.Clear();
+        }
+
+        private void pictureBoxClose_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Exit Applicaton", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cm = new SqlCommand("SELECT * FROM tbUser WHERE username=@username AND password=@password", con);
+                cm.Parameters.AddWithValue("@username", txtName.Text);
+                cm.Parameters.AddWithValue("@password", txtPass.Text);
+                con.Open();
+                dr = cm.ExecuteReader();
+                dr.Read();
+                if (dr.HasRows)
+                {
+                    MessageBox.Show("Welcome " + dr["fullname"].ToString() + " | ", "ACCESS GRANTED", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MainForm main = new MainForm();
+                    this.Hide();
+                    //main.Show();
+                     
+                    main.ShowDialog();
+                    
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Invalid username or password!", "ACCESS DENITED", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    txtName.Clear();
+                    txtPass.Clear();
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        
+    }
+}
